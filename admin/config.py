7dd469048d33f22e96783d1a6efd9a0bd4dc3c12
guixiandao <guixiandao@126.com>
2012-06-17 11:42:48 +0800

    add app config

--- admin/config.py
+++ admin/config.py
@@ -1,7 +1,21 @@
 #-*- coding:utf-8 -*- 
+
+
+
 #-- db config --
 DB_HOST = "localhost"
 DB_PORT = 3306
 DB_USER = "root"
 DB_PASSWD = "123456"
 DB_NAME = "admindb"
+
+
+
+
+#-- app config --
+DEBUG = True
+SECRET_KEY = "dev_key_of_admin"
+SESSION_COOKIE_NAME = "pastme"
+PERMANENT_SESSION_LIFETIME = 3600 * 24 * 30
+
+SITE_COOKIE = "adminck"
